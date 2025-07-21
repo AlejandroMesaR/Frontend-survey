302e37915336197e20df69881af312eb50be869b
AlejandroMesaR <90115115+AlejandroMesaR@users.noreply.github.com>
2025-07-20 22:18:45 -0500

    mas cambios

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,7 +2,7 @@ import api from './api'
 import type { LoginResponse, RegisterRequest, RegisterResponse } from '../models/token'
 
 export const login = async (email: string, password: string): Promise<LoginResponse> => {
-  console.log(api)
+  console.log("safsafsafas0", api.defaults.baseURL)
   const response = await api.post('/auth/login', { email, password })
   return response.data
 }
